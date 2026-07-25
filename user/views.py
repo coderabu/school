051d51cd18caa9b054237de288d8510b8dc1dde2
abdulloh <abdullohoff5@gmail.com>
2026-07-25 16:11:40 +0500

    add model

--- user/views.py
+++ user/views.py
@@ -4,4 +4,4 @@ from django.views import View
 
 class LandingPageView(View):
     def get(self, request):
-        return render(request, 'main/landing.html')
+        return render(request, 'ceo/teacher/teacher.html')
